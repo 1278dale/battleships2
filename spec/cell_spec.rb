28c7251e04133f22c3f8189ee27ab34f56606204
1278dale <dalestevens1278@gmail.com>
2014-11-11 12:25:21 +0000

    Using Stephens code to push up using Sinatra and Capabara

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,3 +1,27 @@
 # should have content
 # can receive shots on its content
 # knows when it has been hit
+# knows if it has not been shot
+# knows if content has been hit
+require 'cell'
+
+describe Cell do
+  let(:cell){Cell.new}
+  let(:content){double :content, hit!: true}
+  before{cell.content = content}
+
+  it "can have content" do
+    expect(cell.content).to eq content
+  end
+
+  it "can shoot it's content" do
+    expect(content).to receive(:hit!)
+    cell.shoot
+  end
+
+  it "knows when it's been hit" do
+    cell.shoot
+    expect(cell.hit?).to eq true 
+  end
+
+end
--- spec/water_spec.rb
+++ spec/water_spec.rb
@@ -1,1 +1,11 @@
 # should be able to be hit
+require 'water'
+
+describe Water do
+  let(:water){Water.new}
+  it "can be hit" do
+    water.hit!
+    expect(water).to be_hit
+  end
+
+end
